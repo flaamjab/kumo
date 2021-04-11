50e776b9806a40a0541c0f8fdc98b67e51f9e041
flaamjab <flaamjab@gmail.com>
2021-04-12 01:04:45 +0500

    reorder lines in unpacking script

--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -3,16 +3,15 @@
 if [ $# -ne 2 ]; then
     echo "Usage: unpack [input_file.docx] [output_dir]"
 else
-    input_file=$1
-
-    input_file_name=$(basename $input_file)
-    input_file_name=${input_file_name%.*}
-    output_dir=$2/$input_file_name
-
     temp_dir=$(mktemp -d)
 
     unzip -q -o $1 -d $temp_dir
     chmod -R u+r+w $temp_dir
+
+    input_file=$1
+    input_file_name=$(basename $input_file)
+    input_file_name=${input_file_name%.*}
+    output_dir=$2/$input_file_name
 
     rm -rf $output_dir
     mkdir $output_dir
