197321b3cda7b9bedccf572fb66234dbbb581789
flaamjab <flaamjab@gmail.com>
2021-04-12 01:59:52 +0500

    document IRange methods

--- src/Kumo/IRange.cs
+++ src/Kumo/IRange.cs
@@ -4,12 +4,20 @@ namespace Kumo
 {
     public interface IRange
     {
+        /// <summary>Retrieves the text within this <c>Range</c>.</summary>
         public string Text();
 
+        /// <summary>Annotate this <c>Range</c> with <c>annotation</c></summary>
         public void Annotate(Annotation annotation);
 
+        /// <summary>Annotate this <c>Range</c> with <c>annotations</c></summary>
         public void Annotate(IEnumerable<Annotation> annotations);
 
+        /** <summary>
+                Annotate this <c>Range</c> using
+                the provided <c>IAnnotator</c>.
+           </summary>
+        */
         public void Annotate(IAnnotator annotator);
     }
 }
